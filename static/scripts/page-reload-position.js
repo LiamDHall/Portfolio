eabf6811eb87979e2569d94eac2796c78f61b495
LiamDHall <hall.d.liam@gmail.com>
2021-08-16 14:11:09 +0000

    Fix mobile scroll position when form submission
    After the contact form is submitted the scroll position should scroll
    to the top of the form instead it reloaded the page at very top of the
    site or at the point where the user submitted the form.
    
    Due to the function controlling the scroll position triggering
    quicker than the target scroll position could be set. When the scroll
    position was returned to the very top of the site it was due to the
    anchor point extension being different in the url of the rendered page
    after the form is submitted.

--- static/scripts/page-reload-position.js
+++ static/scripts/page-reload-position.js
@@ -1,16 +1,16 @@
 // Keeps scroll position in place on page reload 
 document.addEventListener('DOMContentLoaded', function () {
-    var currentPage = sessionStorage.getItem('currentpage');
-    var scrollPos = sessionStorage.getItem('scrollpos');
-    if (currentPage == window.location.href) {
-        if (scrollPos) {
-            window.scrollTo(0, scrollPos);
-            sessionStorage.removeItem('scrollpos');
-        }
-    }
+    setTimeout(function() { scrollToY();}, 100);
 });
+
+function scrollToY() {
+    var scrollPos = sessionStorage.getItem('scrollpos');
+    if (scrollPos) {
+        window.scrollTo(0, scrollPos);
+        sessionStorage.removeItem('scrollpos');
+    }
+}
 
 window.addEventListener('beforeunload', function () {
     sessionStorage.setItem('scrollpos', window.scrollY);
-    sessionStorage.setItem('currentpage', window.location.href);
 });
